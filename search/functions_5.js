1e4395db105a29d6991fc749cdef2c65fcedb031
probonopd <probonopd@users.noreply.github.com>
2023-08-19 17:39:07 +0000

    deploy: dd84b46062a88bd412c573437a28153e6f75465b

--- search/functions_5.js
+++ search/functions_5.js
@@ -3,6 +3,6 @@ var searchData=
   ['geticon_134',['getIcon',['../classDesktopFile.html#a84dfa88bb3ff24916269c79b6e2d8519',1,'DesktopFile']]],
   ['getinstance_135',['getInstance',['../classInfoDialog.html#a9d65468ae3a358eb17ddfc9517111ab7',1,'InfoDialog']]],
   ['getname_136',['getName',['../classDesktopFile.html#a35b351b3070f645de35a6cb35a647679',1,'DesktopFile']]],
-  ['gettrashpath_137',['getTrashPath',['../classTrashHandler.html#a9617e965468d837f330a081ba953bae5',1,'TrashHandler']]],
+  ['gettrashpath_137',['getTrashPath',['../classTrashHandler.html#abea475c1f1ddb922d123070aa9baed85',1,'TrashHandler']]],
   ['getvalue_138',['getValue',['../classDesktopFile.html#aa2ba7c7df857e221116e94bd4aebe191',1,'DesktopFile']]]
 ];
