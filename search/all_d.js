dc3a2749fa80aa12873ff42aaf2805767903b3c6
probonopd <probonopd@users.noreply.github.com>
2023-08-19 10:57:08 +0000

    deploy: 8d5c2e3e13d8ea54197789360003955bdc683914

--- search/all_d.js
+++ search/all_d.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['trashhandler_78',['TrashHandler',['../classTrashHandler.html',1,'TrashHandler'],['../classTrashHandler.html#a893bc441acaf7d135e4d6aac07fcae07',1,'TrashHandler::TrashHandler()']]],
-  ['type_79',['Type',['../classApplicationBundle.html#ad3f6ead97dd4f556572804fba8ff290a',1,'ApplicationBundle']]],
-  ['type_80',['type',['../classApplicationBundle.html#a3fc6017dc262a90075dc0f63863957f4',1,'ApplicationBundle']]],
+  ['type_79',['type',['../classApplicationBundle.html#a3fc6017dc262a90075dc0f63863957f4',1,'ApplicationBundle']]],
+  ['type_80',['Type',['../classApplicationBundle.html#ad3f6ead97dd4f556572804fba8ff290a',1,'ApplicationBundle']]],
   ['typename_81',['typeName',['../classApplicationBundle.html#aa63533713787422b51008cd1a38ed60e',1,'ApplicationBundle']]]
 ];
